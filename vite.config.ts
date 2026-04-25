30744738753d6db60e74acdd18c3ce4826fee48e
chaoticloot <naur.game@gmail.com>
2026-04-25 21:46:51 +0300

    Update vite.config.ts
    path fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function characterPlugin() {
     },
     load(id: string) {
       if (id === resolvedVirtualModuleId) {
-        const charactersDir = path.resolve(__dirname, "public/characters");
+        const charactersDir = path.resolve(__dirname, "characters");
         let files: string[] = [];
         try {
           files = fs
